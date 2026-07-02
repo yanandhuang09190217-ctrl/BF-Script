c7fd5449d8248d527e02b97a2a03b6fb6e5f5acb
yanandhuang09190217-ctrl <yanandhuang09190217@gmail.com>
2026-07-03 03:44:46 +0800

    Create sea1.lua

---
+++ data/sea1.lua
@@ -0,0 +1,35 @@
+-- [[ 一海獨立座標數據庫 ]]
+local Sea1_Database = {
+    {
+        LevelReq = 1,
+        MaxLevel = 9,
+        NPC_Name = "Bandit Quest Giver",
+        NPC_Pos = CFrame.new(1059.5, 16.2, 1549.3),
+        Quest_Name = "BanditQuest1",
+        Quest_Level = 1,
+        Mob_Name = "Bandit",
+        Mob_Pos = CFrame.new(1148.2, 16.5, 1622.8)
+    },
+    {
+        LevelReq = 10,
+        MaxLevel = 14,
+        NPC_Name = "Jungle Quest Giver",
+        NPC_Pos = CFrame.new(-1598.1, 36.8, 153.2),
+        Quest_Name = "JungleQuest",
+        Quest_Level = 1,
+        Mob_Name = "Monkey",
+        Mob_Pos = CFrame.new(-1620.4, 20.3, 140.7)
+    },
+    {
+        LevelReq = 15,
+        MaxLevel = 29,
+        NPC_Name = "Jungle Quest Giver",
+        NPC_Pos = CFrame.new(-1598.1, 36.8, 153.2),
+        Quest_Name = "JungleQuest",
+        Quest_Level = 2,
+        Mob_Name = "Gorilla",
+        Mob_Pos = CFrame.new(-1240.5, 16.2, 490.3)
+    }
+}
+
+return Sea1_Database
