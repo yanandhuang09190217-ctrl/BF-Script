ae1654e01fa4a3643a8aff7b44e0e6305eb106b8
yanandhuang09190217-ctrl <yanandhuang09190217@gmail.com>
2026-07-03 03:48:00 +0800

    Create sea2.lua

---
+++ data/sea2.lua
@@ -0,0 +1,71 @@
+-- [[ 二海 (Second Sea) 獨立座標數據庫 ]]
+local Sea2_Database = {
+    -- 🌹 [700 ~ 724] 玫瑰王國 (第一區)
+    {
+        LevelReq = 700,
+        MaxLevel = 724,
+        NPC_Name = "Quest Giver",
+        NPC_Pos = CFrame.new(-425.5, 73.2, 2151.7),
+        Quest_Name = "Area1Quest",
+        Quest_Level = 1,
+        Mob_Name = "Raider",
+        Mob_Pos = CFrame.new(-745.2, 38.4, 2390.1)
+    },
+    -- 🌹 [725 ~ 774] 玫瑰王國 (第一區)
+    {
+        LevelReq = 725,
+        MaxLevel = 774,
+        NPC_Name = "Quest Giver",
+        NPC_Pos = CFrame.new(-425.5, 73.2, 2151.7),
+        Quest_Name = "Area1Quest",
+        Quest_Level = 2,
+        Mob_Name = "Mercenary",
+        Mob_Pos = CFrame.new(-875.8, 43.1, 2132.5)
+    },
+    -- 🏭 [775 ~ 799] 玫瑰王國 (第二區)
+    {
+        LevelReq = 775,
+        MaxLevel = 799,
+        NPC_Name = "Quest Giver 2",
+        NPC_Pos = CFrame.new(895.1, 114.2, 2568.3),
+        Quest_Name = "Area2Quest",
+        Quest_Level = 1,
+        Mob_Name = "Factory Staff",
+        Mob_Pos = CFrame.new(1048.4, 120.3, 2673.8)
+    },
+    -- 🏭 [800 ~ 874] 玫瑰王國 (第二區)
+    {
+        LevelReq = 800,
+        MaxLevel = 874,
+        NPC_Name = "Quest Giver 2",
+        NPC_Pos = CFrame.new(895.1, 114.2, 2568.3),
+        Quest_Name = "Area2Quest",
+        Quest_Level = 2,
+        Mob_Name = "Marine Captain",
+        Mob_Pos = CFrame.new(882.2, 118.5, 3108.6)
+    },
+    -- 🌿 [875 ~ 949] 綠色地帶 (Green Zone)
+    {
+        LevelReq = 875,
+        MaxLevel = 949,
+        NPC_Name = "Marine Quest Giver",
+        NPC_Pos = CFrame.new(-2440.4, 73.1, -3198.8),
+        Quest_Name = "MarineQuest",
+        Quest_Level = 1,
+        Mob_Name = "Marine Commodore",
+        Mob_Pos = CFrame.new(-2615.1, 73.2, -2954.2)
+    },
+    -- 🧟 [950 ~ 999] 墓地島 (Graveyard)
+    {
+        LevelReq = 950,
+        MaxLevel = 999,
+        NPC_Name = "Crew Captain",
+        NPC_Pos = CFrame.new(-1499.5, 38.6, -2824.1),
+        Quest_Name = "ZombieQuest",
+        Quest_Level = 1,
+        Mob_Name = "Zombie",
+        Mob_Pos = CFrame.new(-1510.3, 38.6, -2944.5)
+    }
+}
+
+return Sea2_Database
