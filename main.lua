7140fc05c4d21461e8596a18ce7eeafa6cc806b4
yanandhuang09190217-ctrl <yanandhuang09190217@gmail.com>
2026-07-03 05:01:00 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,10 @@
--- [[ AhKai Hub 終極版主程式 - 搭載透視與密碼系統 ]]
+-- [[ AhKai Hub 終極版主程式 - 搭載透視與密碼系統 (修復版) ]]
 if not game:IsLoaded() then game.Loaded:Wait() end
 
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer -- 👉 就是漏了這行，這行是救命關鍵！
+
 -- 1. 防斷線系統 (Anti-AFK) 
--- 讓你掛機睡覺時不會被系統踢掉
 local VirtualUser = game:GetService("VirtualUser")
 LocalPlayer.Idled:Connect(function()
     VirtualUser:CaptureController()
@@ -21,10 +23,7 @@ _G.AhKai_Controls = {
 }
 
 -- 你的 GitHub 資訊
-local Git_User = "yanandhuang09190217-ctrl" 
-local Git_Repo = "BF-Script"
-local Git_Branch = "main"
-local GitHub_Base = string.format("https://raw.githubusercontent.com/%s/%s/%s/", Git_User, Git_Repo, Git_Branch)
+local GitHub_Base = "https://raw.githubusercontent.com/yanandhuang09190217-ctrl/BF-Script/main/"
 
 -- 載入頂級 Rayfield UI
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
@@ -36,11 +35,9 @@ local Window = Rayfield:CreateWindow({
     LoadingSubtitle = "By AhKai God",
     ConfigurationSaving = { Enabled = false },
     Discord = {
-        Enabled = true,
-        Invite = "ahkai-hub", -- 這裡可以換成你的 Discord 群組
-        RememberJoins = true
+        Enabled = false,
     },
-    KeySystem = true, -- 啟動密碼系統！逼格拉滿
+    KeySystem = true,
     KeySettings = {
         Title = "AhKai Hub 登入驗證",
         Subtitle = "請輸入高級會員密碼",
@@ -48,11 +45,11 @@ local Window = Rayfield:CreateWindow({
         FileName = "AhKaiKey",
         SaveKey = true,
         GrabKeyFromSite = false, 
-        Key = {"AhKaiGod2026"} -- 只有輸入這個密碼才能使用外掛
+        Key = {"AhKaiGod2026"}
     }
 })
 
--- 動態加載資料庫與引擎 (沿用之前的穩定架構)
+-- 動態加載資料庫與引擎
 local PlaceIds = { [2753915549] = "sea1", [4442272183] = "sea2", [7449423635] = "sea3" }
 local CurrentSeaKey = PlaceIds[game.PlaceId] or "sea1"
 
@@ -81,7 +78,6 @@ Tab_Farm:CreateDropdown({
 
 -- ================== 標籤頁 2: 上帝視角 (ESP) ==================
 local Tab_Visuals = Window:CreateTab("👁️ 視覺透視", 4483362458)
-local RunService = game:GetService("RunService")
 
 Tab_Visuals:CreateToggle({
     Name = "玩家透視 (Player ESP)",
@@ -89,9 +85,8 @@ Tab_Visuals:CreateToggle({
     Flag = "Toggle_ESP_Player",
     Callback = function(Value)
         _G.AhKai_Controls.ESP_Players = Value
-        -- 這裡會觸發簡單的 ESP 高光 (Highlight) 效果
         for _, v in pairs(game.Players:GetChildren()) do
-            if v ~= game.Players.LocalPlayer and v.Character then
+            if v ~= LocalPlayer and v.Character then
                 if Value then
                     local hl = Instance.new("Highlight")
                     hl.Name = "AhKai_ESP"
@@ -100,30 +95,6 @@ Tab_Visuals:CreateToggle({
                 else
                     if v.Character:FindFirstChild("AhKai_ESP") then
                         v.Character.AhKai_ESP:Destroy()
-                    end
-                end
-            end
-        end
-    end,
-})
-
-Tab_Visuals:CreateToggle({
-    Name = "惡魔果實透視 (Fruit ESP)",
-    CurrentValue = false,
-    Flag = "Toggle_ESP_Fruit",
-    Callback = function(Value)
-        _G.AhKai_Controls.ESP_Fruits = Value
-        -- 尋找地圖上的果實並加上高光
-        for _, v in pairs(workspace:GetDescendants()) do
-            if v:IsA("Tool") and string.find(v.Name, "Fruit") then
-                if Value then
-                    local hl = Instance.new("Highlight")
-                    hl.Name = "AhKai_FruitESP"
-                    hl.FillColor = Color3.fromRGB(255, 215, 0) -- 金色
-                    hl.Parent = v
-                else
-                    if v:FindFirstChild("AhKai_FruitESP") then
-                        v.AhKai_FruitESP:Destroy()
                     end
                 end
             end
