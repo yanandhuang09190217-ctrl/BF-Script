ea747788fb22758c14ec648875f05b3a49ec0b3d
yanandhuang09190217-ctrl <yanandhuang09190217@gmail.com>
2026-07-03 03:46:54 +0800

    Create sea3.lua

---
+++ data/sea3.lua
@@ -0,0 +1,60 @@
+-- [[ 三海 (Third Sea) 獨立座標數據庫 ]]
+local Sea3_Database = {
+    -- ⚓ [1500 ~ 1524] 港口鎮 (Port Town)
+    {
+        LevelReq = 1500,
+        MaxLevel = 1524,
+        NPC_Name = "Pirate Port Quest Giver",
+        NPC_Pos = CFrame.new(-290, 44, 5581),
+        Quest_Name = "PiratePortQuest",
+        Quest_Level = 1,
+        Mob_Name = "Pirate Millionaire",
+        Mob_Pos = CFrame.new(-373, 74, 5406)
+    },
+    -- ⚓ [1525 ~ 1574] 港口鎮 (Port Town)
+    {
+        LevelReq = 1525,
+        MaxLevel = 1574,
+        NPC_Name = "Pirate Port Quest Giver",
+        NPC_Pos = CFrame.new(-290, 44, 5581),
+        Quest_Name = "PiratePortQuest",
+        Quest_Level = 2,
+        Mob_Name = "Pistol Billionaire",
+        Mob_Pos = CFrame.new(-469, 74, 5904)
+    },
+    -- 🐍 [1575 ~ 1599] 九頭蛇島 (Hydra Island)
+    {
+        LevelReq = 1575,
+        MaxLevel = 1599,
+        NPC_Name = "Amazon Quest Giver",
+        NPC_Pos = CFrame.new(5722, 601, -253),
+        Quest_Name = "AmazonQuest",
+        Quest_Level = 1,
+        Mob_Name = "Dragon Crew Warrior",
+        Mob_Pos = CFrame.new(6392, 601, -116)
+    },
+    -- 🐍 [1600 ~ 1624] 九頭蛇島 (Hydra Island)
+    {
+        LevelReq = 1600,
+        MaxLevel = 1624,
+        NPC_Name = "Amazon Quest Giver",
+        NPC_Pos = CFrame.new(5722, 601, -253),
+        Quest_Name = "AmazonQuest",
+        Quest_Level = 2,
+        Mob_Name = "Dragon Crew Archer",
+        Mob_Pos = CFrame.new(6590, 601, -66)
+    },
+    -- 🌳 [1625 ~ 1699] 巨樹島 (Great Tree)
+    {
+        LevelReq = 1625,
+        MaxLevel = 1699,
+        NPC_Name = "Marine Commodore Quest Giver",
+        NPC_Pos = CFrame.new(2453, 73, -2026),
+        Quest_Name = "MarineTreeQuest",
+        Quest_Level = 1,
+        Mob_Name = "Marine Commodore",
+        Mob_Pos = CFrame.new(2462, 73, -1949)
+    }
+}
+
+return Sea3_Database
